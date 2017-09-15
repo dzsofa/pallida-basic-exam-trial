b614083d18984ae2bae59df77c10d49c55acac35
gy0p4k <gyorgypakozdi@gmail.com>
2017-09-15 12:27:08 +0200

    fix(namefromemail): fix java syntax

--- namefromemail/NameFromEmail.java
+++ namefromemail/NameFromEmail.java
@@ -1,7 +1,7 @@
 
-class NameFromEmail{
+public class NameFromEmail{
         
-	static void main(string[] args){
+	public static void main(string[] args){
             // Create a function that takes an email address as input in the following format: firstName.lastName@exam.com
             //and returns a string that represents the user name in the following format: lastName firstName
             //example: "elek.viz@exam.com" for this input the output should be: "Viz Elek"

9b4762fe73e4738d3da323f37c73ee28efe9054d
gy0p4k <gyorgypakozdi@gmail.com>
2017-09-16 18:26:14 +0200

    feat(exam): add 4. exercise in py

---
+++ urlsfromhandles/urls_from_handles.py
@@ -0,0 +1,12 @@
+def urls_from_handles(names):
+	pass
+
+# Create a function that takes a list of GitHub handles as input and returns a list of strings that represents
+# GitHub url's under Green Fox Academy organization in the following format: "https://github.com/greenfox-academy/teststudent"
+
+# example:
+# input: ["ghhandle1", "ghhandle2"]
+# output: ["https://github.com/greenfox-academy/ghhandle1", "https://github.com/greenfox-academy/ghhandle2"]
+
+names = ["ghhandle1", "ghhandle2"]
+print(urls_from_handles(names))

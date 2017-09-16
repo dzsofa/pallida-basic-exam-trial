bf7ae2fc3364febb1868a50a6e4065aa34d61c1b
gy0p4k <gyorgypakozdi@gmail.com>
2017-09-16 18:30:08 +0200

    feat(exam): add 4. exercise in cs

---
+++ urlsfromhandles/UrlsFromHandles.cs
@@ -0,0 +1,15 @@
+namespace UrlsFromHandles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a function that takes a list of GitHub handles as input and returns a list of strings that represents
+            // GitHub url's under Green Fox Academy organization in the following format: "https://github.com/greenfox-academy/teststudent"
+
+            // example:
+            // input: ["ghhandle1", "ghhandle2"]
+            // output: ["https://github.com/greenfox-academy/ghhandle1", "https://github.com/greenfox-academy/ghhandle2"]
+        }
+    }
+}

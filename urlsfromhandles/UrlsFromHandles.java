14f5a365bf2e91122a314456c79224b12a2518a7
gy0p4k <gyorgypakozdi@gmail.com>
2017-09-16 18:28:35 +0200

    feat(exam): add 4. exercise in java

---
+++ urlsfromhandles/UrlsFromHandles.java
@@ -0,0 +1,12 @@
+
+public class UrlFromHandles{
+	
+	public static void main(string[] args){
+			// Create a function that takes a list of GitHub handles as input and returns a list of strings that represents
+			// GitHub url's under Green Fox Academy organization in the following format: "https://github.com/greenfox-academy/teststudent"
+
+			// example:
+			// input: ["ghhandle1", "ghhandle2"]
+			// output: ["https://github.com/greenfox-academy/ghhandle1", "https://github.com/greenfox-academy/ghhandle2"]
+        }
+}

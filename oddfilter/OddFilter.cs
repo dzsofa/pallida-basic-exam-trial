647d67fa236a99e4bbb293f9ae38aec002429a68
kondfox <peter.tamas@greenfoxacademy.com>
2017-09-17 16:10:53 +0200

    fix(OddFilter): C# initial code fixed, because Console.WriteLine won't print the content of a list

--- oddfilter/OddFilter.cs
+++ oddfilter/OddFilter.cs
@@ -7,10 +7,22 @@ namespace OddFilter
     {
         static void Main(string[] args)
         {
-            // Create a function that takes a list as a parameter,
+            // Create an OddFilter function that takes a list as a parameter,
             // and returns a new list with every odd element from the orignal list
-            Console.WriteLine(OddFilter(new List<int>  { 1, 2, 3, 4, 5 }));
-            // should print [1, 3, 5]
+            List<int> oddElements = OddFilter(new List<int> { 1, 2, 3, 4, 5 });
+
+            // In case of the example input above, the given PrintList function should print 1 3 5 
+            PrintList(oddElements);
+
+            Console.ReadLine();
+        }
+
+        private static void PrintList(List<int> list)
+        {
+            foreach (int element in list)
+            {
+                Console.Write("{0} ", element);
+            }
         }
     }
 }

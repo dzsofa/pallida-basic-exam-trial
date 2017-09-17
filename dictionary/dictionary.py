5e64ac0695621566278ee04585f5765459796606
aze <adam.gyulavari@lab.coop>
2017-09-17 11:43:11 +0200

    fix(dictionary) remove unnecessary empty lines from python

--- dictionary/dictionary.py
+++ dictionary/dictionary.py
@@ -3,18 +3,15 @@ dictionary = [
     {"fa": "tree"}
 ]
 
-
 # Implement this method. It should add the given key-value pair to the
 # list 'dictionary'
 def add_word(hun_word, eng_word):
     pass
 
-
 # Implement these methods. They should return the translation of the given
 # word form the list 'dictionary'
 def translate_to_hun(eng_word):
     pass
-
 
 def translate_to_eng(hun_word):
     pass

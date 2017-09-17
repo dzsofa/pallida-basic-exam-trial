268600e9c64fbad8e5784658fee60cf92374b63b
aze <adam.gyulavari@lab.coop>
2017-09-17 11:56:10 +0200

    fix(dictionary) more syntax and semantic errors

--- dictionary/Dictionary.cs
+++ dictionary/Dictionary.cs
@@ -2,9 +2,11 @@ namespace Dictionary
 {
     class Program
     {
+        static Dictionary<string, string> dictionary;
+        
         static void Main(string[] args)
         {
-            Dictionary<string, string> dictionary = new Dictionary<string, string>
+            dictionary = new Dictionary<string, string>;
             {
                 { "alma", "apple" },
                 { "fa", "tree" }
@@ -13,10 +15,30 @@ namespace Dictionary
             //add more words to your dictionary
 
         }
+
+
+        // Implement this method. It should add the given key-value pair to the the dictionary
+        public static void AddWord(string hunWord, string engWord)
+        {
+
+        }
+        
+        // Implement this method. It should remove the key-value pair by the given key from the dictionary
+        public static void RemoveWord(string hunWord)
+        {
+        
+        }
+
         //implement a method which works as a traslator from hungarian to english
         //example: you give it a parameter "alma" and it's output is "tree"
+        public static string TranslateToEng(string hungarian) {
+
+        }
 
         //implement a method which works as a translator from english to hungarian
         //example: you give it a parameter "apple" and it's output is "alma"
+        public static string TranslateToHun(string english) {
+
+        }
     }
 }
--- dictionary/Dictionary.java
+++ dictionary/Dictionary.java
@@ -1,16 +1,23 @@
 public class Dictionary{
+	static Map<String,String> map;
+
 	public static void main(String[] args) {
-		Map<String,String> map = new HashMap<String,String>();   
+		map = new HashMap<String,String>();   
 		map.put("alma", "apple");  
    		map.put("fa", "tree");  
 	}
+
+	// Implement this method. It should add the given key-value pair to the the map
+	public static void addWord(String hunWord, String engWord) {
+
+	}
 	
-	// Implement this method. It should remove the key-value pair by the given key from the map 'dictionary'
+	// Implement this method. It should remove the key-value pair by the given key from the map
 	public static void removeWord(String hunWord) {
 	
 	}
 
-	// Implement these methods. They should return the translation of the given word from the list 'dictionary'
+	// Implement these methods. They should return the translation of the given word from the map
 	public static String translateToHun(String engWord) {
 		return null;
 	}

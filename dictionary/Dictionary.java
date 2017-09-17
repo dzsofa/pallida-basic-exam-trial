9a183ae15e1262b7290656ccea75a5471f765f95
aze <adam.gyulavari@lab.coop>
2017-09-17 11:41:13 +0200

    fix(dictionary) java styling and syntax errors

--- dictionary/Dictionary.java
+++ dictionary/Dictionary.java
@@ -1,21 +1,21 @@
 public class Dictionary{
-	public static void main(String[] args){
-		Map<String,String> dictionary=new HashMap<String,String>();   
-			map.put("alma", "apple");  
-   			map.put("fa", "tree");  
+	public static void main(String[] args) {
+		Map<String,String> map = new HashMap<String,String>();   
+		map.put("alma", "apple");  
+   		map.put("fa", "tree");  
 	}
 	
 	// Implement this method. It should remove the key-value pair by the given key from the map 'dictionary'
-	public static void removeWord(hunWord){
+	public static void removeWord(String hunWord) {
 	
 	}
 
 	// Implement these methods. They should return the translation of the given word from the list 'dictionary'
-	public static String translateToHun(engWord){
+	public static String translateToHun(String engWord) {
 	
 	}
 
-  	public static String translateToEng(hunWord){
+  	public static String translateToEng(String hunWord) {
 	
   	}
 }

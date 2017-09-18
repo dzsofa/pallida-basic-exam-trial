5bb2593f9417486d262ee8f70c4af030d18a5da4
Zsofia <zsofi.dedeo@mensa.hu>
2017-09-18 10:12:41 +0200

    Dictionary addWord method done, tested

---
+++ dictionary/src/Dictionary.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Dictionary {
+    static Map<String,String> map;
+
+    public static void main(String[] args) {
+        map = new HashMap<String,String>();
+        map.put("alma", "apple");
+        map.put("fa", "tree");
+        addWord("kenyer", "bread");
+        System.out.println(Arrays.asList(map));
+    }
+
+    // Implement this method. It should add the given key-value pair to the the map
+    public static void addWord(String hunWord, String engWord) {
+        map.put(hunWord, engWord);
+    }
+
+    // Implement this method. It should remove the key-value pair by the given key from the map
+    public static void removeWord(String hunWord) {
+
+    }
+
+    // Implement these methods. They should return the translation of the given word from the map
+    public static String translateToHun(String engWord) {
+        return null;
+    }
+
+    public static String translateToEng(String hunWord) {
+        return null;
+    }
+}

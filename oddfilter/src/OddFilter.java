23783f2d40f58a6d1f8ac7e8cb2815c6d5a4c11d
Zsofia <zsofi.dedeo@mensa.hu>
2017-09-18 10:03:32 +0200

    OddFilter finished

---
+++ oddfilter/src/OddFilter.java
@@ -0,0 +1,23 @@
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class OddFilter {
+    public static void main(String[] args) {
+        // Create a function that takes a list as a parameter,
+        // and returns a new list with every odd element from the orignal list
+
+        ArrayList<Integer> myNumbers = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5));
+        System.out.println(oddFilter(myNumbers));
+        // should print [1, 3, 5]
+    }
+
+    public static ArrayList<Integer> oddFilter(ArrayList<Integer> sortable) {
+        for (int i = 0; i < sortable.size(); i++) {
+            if (sortable.get(i) % 2 == 0) {
+                sortable.remove(i);
+            }
+        }
+        return sortable;
+    }
+}

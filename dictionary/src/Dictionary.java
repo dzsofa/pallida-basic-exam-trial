ea7ffd06152f8caacc3d3becdf5efed687b607a7
Zsofia <zsofi.dedeo@mensa.hu>
2017-09-18 12:23:55 +0200

    Dictionary TranslateToHun done, some comments added

--- dictionary/src/Dictionary.java
+++ dictionary/src/Dictionary.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class Dictionary {
     static Map<String, String> map;
@@ -9,13 +10,24 @@ public class Dictionary {
         map = new HashMap<String, String>();
         map.put("alma", "apple");
         map.put("fa", "tree");
+
+        //adding new key-value pair to hashmap
         addWord("kenyer", "bread");
         System.out.println(Arrays.asList(map));
+        System.out.println();
+
+        //removing entry from hashmap
         removeWord("fa");
         System.out.println(Arrays.asList(map));
-        translateToHun("bread");
+        System.out.println();
+
+        //translate from English to Hungarian
         System.out.println(translateToHun("apple"));
+        System.out.println();
+
+        //translate from Hungarian to English
         System.out.println(translateToEng("alma"));
+        System.out.println();
     }
 
     // Implement this method. It should add the given key-value pair to the the map
@@ -31,9 +43,12 @@ public class Dictionary {
 
     // Implement these methods. They should return the translation of the given word from the map
     public static String translateToHun(String engWord) {
-        String key = "";
-        key = map.get(engWord);
-        return key;
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            if (Objects.equals(engWord, entry.getValue())) {
+                return entry.getKey();
+            }
+        }
+        return null;
     }
 
     public static String translateToEng(String hunWord) {

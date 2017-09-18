d77303161682d969404c34000fe0bc44e86a965a
Zsofia <zsofi.dedeo@mensa.hu>
2017-09-18 10:45:12 +0200

    UrlsFromHandles done

--- dictionary/src/Dictionary.java
+++ dictionary/src/Dictionary.java
@@ -3,15 +3,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Dictionary {
-    static Map<String,String> map;
+    static Map<String, String> map;
 
     public static void main(String[] args) {
-        map = new HashMap<String,String>();
+        map = new HashMap<String, String>();
         map.put("alma", "apple");
         map.put("fa", "tree");
         addWord("kenyer", "bread");
         System.out.println(Arrays.asList(map));
         removeWord("fa");
+        System.out.println(Arrays.asList(map));
+        translateToHun("bread");
         System.out.println(Arrays.asList(map));
     }
 
@@ -28,6 +30,11 @@ public class Dictionary {
 
     // Implement these methods. They should return the translation of the given word from the map
     public static String translateToHun(String engWord) {
+        for (Map.Entry<String, String> e : map.entrySet()) {
+            if (map.get(engWord).equals(map.get(e))) {
+                return map.get(e);
+            }
+        }
         return null;
     }
 
---
+++ urlsfromhandles/src/UrlsFromHandles.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class UrlsFromHandles {
+    public static void main(String[] args){
+        // Create a function that takes a list of GitHub handles as input and returns a list of strings that represents
+        // GitHub url's under Green Fox Academy organization in the following format: "https://github.com/greenfox-academy/teststudent"
+
+        // example:
+        // input: ["ghhandle1", "ghhandle2"]
+        // output: ["https://github.com/greenfox-academy/ghhandle1", "https://github.com/greenfox-academy/ghhandle2"]
+        ArrayList<String> names = new ArrayList<>(Arrays.asList("ghhandle1", "ghhandle2"));
+        System.out.println(urlsFromHandles(names));
+    }
+
+    public static ArrayList<String> urlsFromHandles(ArrayList<String> nameInput) {
+        for (int i = 0; i < nameInput.size(); i++) {
+            nameInput.set(i, "https://github.com/greenfox-academy/" + nameInput.get(i));
+        }
+        return nameInput;
+    }
+}
